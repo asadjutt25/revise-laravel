58784b8bc93ae569f554a8a0f5ae0faa140eb3f9
asadjutt25 <asadshabbir0025@gmail.com>
2025-12-30 15:48:52 +0500

    display-db-data foreach-ui-list

--- app/Http/Controllers/user26controller.php
+++ app/Http/Controllers/user26controller.php
@@ -7,7 +7,12 @@ use Illuminate\Support\Facades\DB;
 
 class user26controller extends Controller
 {
-    function users(){
-        return DB::select('select * from users');
+    // function users(){
+    //     return DB::select('select * from users');
+    // }
+
+     function users(){
+        $users= DB::select('select * from users');
+        return view('users27',['users'=>$users]);
     }
 }
---
+++ resources/views/users27.blade.php
@@ -0,0 +1,35 @@
+<div>
+   <h1>users list</h1>
+   {{-- {{  print_r($users) }} --}}
+
+<table border="1">
+    <tr>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Phone</td>
+    </tr>
+    @foreach ($users as $user )
+    <tr>
+        <td>{{ $user->name }}</td>
+         <td>{{ $user->email }}</td>
+          <td>{{ $user->phone }}</td>
+    </tr>
+        
+    @endforeach
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+</div>
